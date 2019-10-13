529f86ac290b64d621e347b1186744efbafb5c1f
Christian Moll <christian@chrmoll.de>
2019-10-13 14:39:39 +0200

    small rearange of vars

--- barcode-scanner.js
+++ barcode-scanner.js
@@ -1,17 +1,8 @@
-// var KeyboardLines = require('node-hid-stream').KeyboardLines;
-// var lines = new KeyboardLines({ vendorId: 0x2010, productId: 0x7638 });
-//
-// lines.on("data", function(data) {
-//   // The user has pressed w, a, s & d, ENTER (simultaneously (why? I don't know))
-//   console.log(data); //  "wasd"
-// });
-//
-
+"use strict";
+var KeyboardLines = require('node-hid-stream').KeyboardLines;
+var lines = new KeyboardLines({ vendorId: 0x2010, productId: 0x7638 });
 
 module.exports = function(RED) {
-	"use strict";
-	var KeyboardLines = require('node-hid-stream').KeyboardLines;
-	var lines = new KeyboardLines({ vendorId: 0x2010, productId: 0x7638 });
 
 	function BarcodeScannerNode(config) {
   	RED.nodes.createNode(this, config);
